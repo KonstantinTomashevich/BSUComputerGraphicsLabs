68da2369e4c9bdbc4d6a9320441686e9d9f4ab59
Konstantin Tomashevich <ekit.mail@gmail.com>
2020-05-18 18:35:04 +0300

    Split main program to functions.

--- Lab6/Main.cpp
+++ Lab6/Main.cpp
@@ -23,9 +23,64 @@
 struct
 {
     double deltaTime = 0.0;
-    AbstractCamera *activeCamera = nullptr;
     ImGuiIO *imGuiIO_ = nullptr;
+    GLFWwindow *window = nullptr;
+
+    AbstractCamera *activeCamera = nullptr;
+    Shader *shader = nullptr;
+    AbstractMaterial *material = nullptr;
+    AbstractCamera *perspectiveCamera = nullptr;
+
+    AbstractGeometry *symbolGeometry = nullptr;
+    AbstractGeometry *gridGeometry = nullptr;
+
+    Drawable *symbolDrawable = nullptr;
+    Drawable *gridOXYDrawable = nullptr;
+    Drawable *gridOXZDrawable = nullptr;
+    Drawable *gridOYZDrawable = nullptr;
 } Context;
+
+static void ErrorCallback (int error, const char *description);
+static void KeyCallback (GLFWwindow *window, int key, int scancode, int action, int mods);
+static void MoveCamera (int xDir, int yDir, int zDir);
+static void MatrixToString (const glm::mat4x4 &matrix, std::string &string);
+
+static void FreeResources ();
+static void SetupScene ();
+static void SetupImGUI ();
+static void SetupGLFW ();
+
+static void RenderScene(const glm::mat4x4 &projection);
+static void UpdateCamera ();
+static void UpdateViewport (glm::mat4x4 &projectionOutput);
+static void RenderUI (const glm::mat4x4 &projection);
+
+int main (int argumentCount, char **arguments)
+{
+    SetupGLFW ();
+    SetupImGUI ();
+    SetupScene ();
+
+    double lastFrameTime = glfwGetTime ();
+    while (!glfwWindowShouldClose (Context.window))
+    {
+        double currentFrameTime = glfwGetTime ();
+        Context.deltaTime = currentFrameTime - lastFrameTime;
+        lastFrameTime = currentFrameTime;
+
+        glm::mat4x4 projection;
+        UpdateViewport (projection);
+        RenderScene (projection);
+        RenderUI (projection);
+
+        glfwSwapBuffers (Context.window);
+        glfwPollEvents ();
+        UpdateCamera ();
+    }
+
+    FreeResources ();
+    exit (EXIT_SUCCESS);
+}
 
 static void ErrorCallback (int error, const char *description)
 {
@@ -58,16 +113,72 @@ static void MatrixToString (const glm::mat4x4 &matrix, std::string &string)
     {
         for (unsigned int col = 0; col < 4; ++col)
         {
-            string += std::to_string (value [row * 4 + col]) + " ";
+            string += std::to_string (value[row * 4 + col]) + " ";
         }
 
         string += "\n";
     }
 }
 
-int main (int argumentCount, char **arguments)
+static void FreeResources ()
 {
-    GLFWwindow *window;
+    delete Context.shader;
+    delete Context.material;
+    delete Context.perspectiveCamera;
+
+    delete Context.symbolGeometry;
+    delete Context.gridGeometry;
+
+    delete Context.symbolDrawable;
+    delete Context.gridOXYDrawable;
+    delete Context.gridOXZDrawable;
+    delete Context.gridOYZDrawable;
+
+    ImGui_ImplOpenGL3_Shutdown ();
+    ImGui_ImplGlfw_Shutdown ();
+    ImGui::DestroyContext ();
+
+    glfwDestroyWindow (Context.window);
+    glfwTerminate ();
+}
+
+static void SetupScene ()
+{
+    Context.shader = new Shader (vertexColorVertexShader, vertexColorFragmentShader);
+    Context.material = new EmptyMaterial ();
+    Context.material->SetLinkedShader (Context.shader);
+
+    Context.gridGeometry = Generate2DGrid (21, 21, {150, 0, 0});
+    Context.gridOXYDrawable = new Drawable ();
+    Context.gridOXYDrawable->SetLinkedGeometry (Context.gridGeometry);
+    Context.gridOXYDrawable->SetLinkedMaterial (Context.material);
+    Context.gridOXYDrawable->SetLocalScale ({10.0f, 10.0f, 10.0f});
+
+    Context.symbolGeometry = GenerateTSymbol ();
+    Context.symbolDrawable = new Drawable ();
+    Context.symbolDrawable->SetLinkedGeometry (Context.symbolGeometry);
+    Context.symbolDrawable->SetLinkedMaterial (Context.material);
+
+    Context.perspectiveCamera = new PerspectiveCamera (M_PI / 3, 0.01f, 100.0f);
+    Context.perspectiveCamera->SetLocalPosition ({0.0f, -10.0f, 5.0f});
+    Context.perspectiveCamera->SetLocalRotation ({M_PI / 4, 0.0f, 0.0f});
+    Context.activeCamera = Context.perspectiveCamera;
+}
+
+static void SetupImGUI ()
+{
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext ();
+    Context.imGuiIO_ = &ImGui::GetIO ();
+
+    ImGui_ImplGlfw_InitForOpenGL (Context.window, true);
+    ImGui_ImplOpenGL3_Init ("#version 150");
+    ImGui::StyleColorsDark ();
+    Context.imGuiIO_->Fonts->AddFontDefault ();
+}
+
+static void SetupGLFW ()
+{
     glfwSetErrorCallback (ErrorCallback);
 
     if (!glfwInit ())
@@ -78,147 +189,102 @@ int main (int argumentCount, char **arguments)
     glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 0);
 
-    window = glfwCreateWindow (1024, 768, "Konstantin Tomashevich CG Lab 6", NULL, NULL);
-    if (!window)
+    Context.window = glfwCreateWindow (1024, 768,
+        "Konstantin Tomashevich CG Lab 6", NULL, NULL);
+
+    if (!Context.window)
     {
         glfwTerminate ();
         exit (EXIT_FAILURE);
     }
 
-    glfwSetKeyCallback (window, KeyCallback);
-    glfwMakeContextCurrent (window);
+    glfwSetKeyCallback (Context.window, KeyCallback);
+    glfwMakeContextCurrent (Context.window);
     glfwSwapInterval (1);
+
     gladLoadGLLoader ((GLADloadproc) glfwGetProcAddress);
     glEnable (GL_DEPTH_TEST);
+}
 
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext ();
-    Context.imGuiIO_ = &ImGui::GetIO ();
+static void RenderScene (const glm::mat4x4 &projection)
+{
+    Context.gridOXYDrawable->Draw (projection);
+    Context.symbolDrawable->Draw (projection);
+}
 
-    ImGui_ImplGlfw_InitForOpenGL (window, true);
-    ImGui_ImplOpenGL3_Init ("#version 150");
-    ImGui::StyleColorsDark ();
-    Context.imGuiIO_->Fonts->AddFontDefault ();
-
-    auto *shader = new Shader (vertexColorVertexShader, vertexColorFragmentShader);
-    EmptyMaterial material;
-    material.SetLinkedShader (shader);
-
-    AbstractGeometry *gridGeometry = Generate2DGrid (21, 21, {150, 0, 0});
-    auto *gridDrawable = new Drawable ();
-    gridDrawable->SetLinkedGeometry (gridGeometry);
-    gridDrawable->SetLinkedMaterial (&material);
-
-    gridDrawable->SetLocalPosition ({0.0f, 0.0f, 0.0f});
-    gridDrawable->SetLocalScale ({10.0f, 10.0f, 10.0f});
-
-    AbstractGeometry *symbolGeometry = GenerateTSymbol ();
-    auto *symbolDrawable = new Drawable ();
-    symbolDrawable->SetLinkedGeometry (symbolGeometry);
-    symbolDrawable->SetLinkedMaterial (&material);
-
-    auto *camera = new PerspectiveCamera (M_PI / 3, 0.01f, 100.0f);
-    camera->SetLocalPosition ({0.0f, -10.0f, 5.0f});
-    camera->SetLocalRotation ({M_PI / 4, 0.0f, 0.0f});
-    Context.activeCamera = camera;
-
-    double lastFrameTime = glfwGetTime ();
-    while (!glfwWindowShouldClose (window))
+static void UpdateCamera ()
+{
+    if (!Context.imGuiIO_->WantCaptureKeyboard)
     {
-        double currentFrameTime = glfwGetTime ();
-        Context.deltaTime = currentFrameTime - lastFrameTime;
-        lastFrameTime = currentFrameTime;
+        MoveCamera (
+            Context.imGuiIO_->KeysDown[GLFW_KEY_A] ? -1 : (Context.imGuiIO_->KeysDown[GLFW_KEY_D] ? 1 : 0),
+            Context.imGuiIO_->KeysDown[GLFW_KEY_S] ? -1 : (Context.imGuiIO_->KeysDown[GLFW_KEY_W] ? 1 : 0),
+            Context.imGuiIO_->KeysDown[GLFW_KEY_Q] ? -1 : (Context.imGuiIO_->KeysDown[GLFW_KEY_E] ? 1 : 0)
+        );
+    }
+}
 
-        int width, height;
-        glfwGetFramebufferSize (window, &width, &height);
+static void UpdateViewport (glm::mat4x4 &projectionOutput)
+{
+    int width, height;
+    glfwGetFramebufferSize (Context.window, &width, &height);
 
-        glViewport (0, 0, width, height);
-        glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glViewport (0, 0, width, height);
+    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    Context.activeCamera->GetViewMatrix (width, height, projectionOutput);
+}
 
-        if (!Context.imGuiIO_->WantCaptureKeyboard)
+static void RenderUI (const glm::mat4x4 &projection)
+{
+    ImGui_ImplOpenGL3_NewFrame ();
+    ImGui_ImplGlfw_NewFrame ();
+    ImGui::NewFrame ();
+
+    ImGui::SetNextWindowPos (ImVec2 (0, 0), ImGuiCond_Appearing);
+    ImGui::Begin ("Configuration Window");
+
+    if (ImGui::CollapsingHeader ("Transform"))
+    {
+        glm::vec3 position = Context.symbolDrawable->GetLocalPosition ();
+        glm::quat qRotation = Context.symbolDrawable->GetLocalRotation ();
+        glm::vec3 eulerRotation = glm::degrees (glm::eulerAngles (qRotation));
+
+        glm::vec3 scale = Context.symbolDrawable->GetLocalScale ();
+        ImGui::InputFloat3 ("Position: ", glm::value_ptr (position));
+        ImGui::InputFloat4 ("Rotation: ", glm::value_ptr (qRotation));
+
+        if (ImGui::InputFloat3 ("Rotation (euler, degrees): ", glm::value_ptr (eulerRotation)))
         {
-            MoveCamera (
-                Context.imGuiIO_->KeysDown[GLFW_KEY_A] ? -1 : (Context.imGuiIO_->KeysDown[GLFW_KEY_D] ? 1 : 0),
-                Context.imGuiIO_->KeysDown[GLFW_KEY_S] ? -1 : (Context.imGuiIO_->KeysDown[GLFW_KEY_W] ? 1 : 0),
-                Context.imGuiIO_->KeysDown[GLFW_KEY_Q] ? -1 : (Context.imGuiIO_->KeysDown[GLFW_KEY_E] ? 1 : 0)
-            );
+            qRotation = glm::quat (glm::radians (eulerRotation));
         }
 
-        glm::mat4x4 projection = glm::identity <glm::mat4x4> ();
-        camera->GetViewMatrix (width, height, projection);
-
-        gridDrawable->Draw (projection);
-        symbolDrawable->Draw (projection);
-
-        ImGui_ImplOpenGL3_NewFrame ();
-        ImGui_ImplGlfw_NewFrame ();
-        ImGui::NewFrame ();
-
-        ImGui::SetNextWindowPos (ImVec2 (0, 0), ImGuiCond_Appearing);
-        ImGui::Begin ("Configuration Window");
-
-        if (ImGui::CollapsingHeader ("Transform"))
-        {
-            glm::vec3 position = symbolDrawable->GetLocalPosition ();
-            glm::quat qRotation = symbolDrawable->GetLocalRotation ();
-            glm::vec3 eulerRotation = glm::degrees (glm::eulerAngles (qRotation));
-
-            glm::vec3 scale = symbolDrawable->GetLocalScale ();
-            ImGui::InputFloat3 ("Position: ", glm::value_ptr(position));
-            ImGui::InputFloat4 ("Rotation: ", glm::value_ptr(qRotation));
-
-            if (ImGui::InputFloat3 ("Rotation (euler, degrees): ", glm::value_ptr(eulerRotation)))
-            {
-                qRotation = glm::quat (glm::radians(eulerRotation));
-            }
-
-            ImGui::InputFloat3 ("Scale: ", glm::value_ptr(scale));
-            symbolDrawable->SetLocalPosition (position);
-            symbolDrawable->SetLocalRotation (qRotation);
-            symbolDrawable->SetLocalScale (scale);
-        }
-
-        std::string stringBuffer;
-        if (ImGui::CollapsingHeader ("View Space Matrix"))
-        {
-            MatrixToString(projection, stringBuffer);
-            ImGui::Text ("%s", stringBuffer.c_str ());
-        }
-
-        if (ImGui::CollapsingHeader ("Model Space Matrix"))
-        {
-            MatrixToString(symbolDrawable->GetMatrix (), stringBuffer);
-            ImGui::Text ("%s", stringBuffer.c_str ());
-        }
-
-        if (ImGui::CollapsingHeader ("Combined Matrix"))
-        {
-            MatrixToString(projection * symbolDrawable->GetMatrix (), stringBuffer);
-            ImGui::Text ("%s", stringBuffer.c_str ());
-        }
-
-        ImGui::End ();
-        ImGui::Render ();
-        ImGui_ImplOpenGL3_RenderDrawData (ImGui::GetDrawData ());
-
-        glfwSwapBuffers (window);
-        glfwPollEvents ();
+        ImGui::InputFloat3 ("Scale: ", glm::value_ptr (scale));
+        Context.symbolDrawable->SetLocalPosition (position);
+        Context.symbolDrawable->SetLocalRotation (qRotation);
+        Context.symbolDrawable->SetLocalScale (scale);
     }
 
-    delete shader;
-    delete camera;
+    std::string stringBuffer;
+    if (ImGui::CollapsingHeader ("View Space Matrix"))
+    {
+        MatrixToString (projection, stringBuffer);
+        ImGui::Text ("%s", stringBuffer.c_str ());
+    }
 
-    delete gridGeometry;
-    delete symbolGeometry;
+    if (ImGui::CollapsingHeader ("Model Space Matrix"))
+    {
+        MatrixToString (Context.symbolDrawable->GetMatrix (), stringBuffer);
+        ImGui::Text ("%s", stringBuffer.c_str ());
+    }
 
-    delete gridDrawable;
-    delete symbolDrawable;
+    if (ImGui::CollapsingHeader ("Combined Matrix"))
+    {
+        MatrixToString (projection * Context.symbolDrawable->GetMatrix (), stringBuffer);
+        ImGui::Text ("%s", stringBuffer.c_str ());
+    }
 
-    ImGui_ImplOpenGL3_Shutdown ();
-    ImGui_ImplGlfw_Shutdown ();
-    ImGui::DestroyContext ();
-
-    glfwDestroyWindow (window);
-    glfwTerminate ();
-    exit (EXIT_SUCCESS);
+    ImGui::End ();
+    ImGui::Render ();
+    ImGui_ImplOpenGL3_RenderDrawData (ImGui::GetDrawData ());
 }
+
